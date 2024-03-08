09e7ec7902345656f86602371b1301b8b327ef97
Zachery Bingaman <zacherybingaman@gmail.com>
2024-03-08 15:37:31 -0500

    created save_position_helper.h to save planet positions

---
+++ save_position_helper.h
@@ -0,0 +1,12 @@
+/**
+ * Declares all of the position save files.
+ */
+
+#include "matrix.h"
+
+
+/**
+* Calculate the euclidean distance between two bodies in 3D space
+*/
+inline static double euclidean_distance(double* body_1_position, double* body_2_position) {
+}
